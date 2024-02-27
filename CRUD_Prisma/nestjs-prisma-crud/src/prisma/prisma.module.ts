df0cc1e4c8a33e09043d19e9beb7cc93c7eacd4c
Michael Javier <m.mota@concentra.com.do>
2024-02-27 15:13:13 -0400

    delete files

--- CRUD_Prisma/nestjs-prisma-crud/src/prisma/prisma.module.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable prettier/prettier */
-import { Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
-
-@Module({
-  providers: [PrismaService], //lo que yo quiero importar
-  exports: [PrismaService],
-})
-export class PrismaModule {}
