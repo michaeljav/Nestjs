66510d7d576e689e755d84b9958d573ff515c242
Michael Javier <m.mota@concentra.com.do>
2024-02-28 12:08:51 -0400

    creado tabla entity user y  guardado info signup

--- TYPEORM/nestjs-task-management/src/auth/auth.service.ts
+++ TYPEORM/nestjs-task-management/src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { UserEntity } from './entity/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
@@ -9,4 +10,13 @@ export class AuthService {
     @InjectRepository(UserEntity)
     private userRepository: Repository<UserEntity>,
   ) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const user = new UserEntity();
+    user.username = username;
+    user.password = password;
+    await this.userRepository.save(user);
+  }
 }
