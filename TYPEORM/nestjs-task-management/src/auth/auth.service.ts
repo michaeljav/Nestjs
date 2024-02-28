0baf4cc342d647f936f88d7a346aeacc622b8542
Michael Javier <m.mota@concentra.com.do>
2024-02-28 11:31:27 -0400

    creado repository de auth

--- TYPEORM/nestjs-task-management/src/auth/auth.service.ts
+++ TYPEORM/nestjs-task-management/src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { UserEntity } from './entity/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private userRepository: Repository<UserEntity>,
+  ) {}
+}
