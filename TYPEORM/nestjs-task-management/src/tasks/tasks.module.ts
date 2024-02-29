436a007410d46a0eb053142c301be9c6b1524f76
Michael Javier <m.mota@concentra.com.do>
2024-02-28 20:16:15 -0400

    authorization in the controller

--- TYPEORM/nestjs-task-management/src/tasks/tasks.module.ts
+++ TYPEORM/nestjs-task-management/src/tasks/tasks.module.ts
@@ -3,10 +3,11 @@ import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskEntity } from './entities/task.entity';
+import { AuthModule } from 'src/auth/auth.module';
 // import { TaskRepository } from './entities/task.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TaskEntity])],
+  imports: [TypeOrmModule.forFeature([TaskEntity]), AuthModule],
   controllers: [TasksController],
   providers: [TasksService],
 })
