7e27083936b34be29a352084cca19bd99e1eeacb
Michael Javier <m.mota@concentra.com.do>
2024-02-29 09:25:22 -0400

    fRONT-END APPLICATION SETUP DEVELOPMENT MODE

--- TYPEORM/nestjs-task-management/src/main.ts
+++ TYPEORM/nestjs-task-management/src/main.ts
@@ -9,6 +9,9 @@ async function bootstrap() {
   const logger = new Logger('bootstrap');
 
   const app = await NestFactory.create(AppModule);
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  }
 
   console.log(serverConfig);
 
