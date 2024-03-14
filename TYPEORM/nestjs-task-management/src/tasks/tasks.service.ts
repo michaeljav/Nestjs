636c9e8a7ef237c0b250c5529cbd52c75f2c3bed
Michael Javier <m.mota@concentra.com.do>
2024-03-13 20:34:20 -0400

    Ejemplo de transaccion typeorm

--- TYPEORM/nestjs-task-management/src/tasks/tasks.service.ts
+++ TYPEORM/nestjs-task-management/src/tasks/tasks.service.ts
@@ -8,11 +8,12 @@ import {
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { InsertResult, Repository } from 'typeorm';
+import { DataSource, InsertResult, Repository, getRepository } from 'typeorm';
 // import { TaskRepository } from './entities/task.repository';
 import { TaskEntity } from './entities/task.entity';
 import { TaskStatus } from './task-status.enum';
 import { UserEntity } from 'src/auth/entity/user.entity';
+import { throws } from 'assert';
 
 @Injectable()
 export class TasksService {
@@ -20,6 +21,7 @@ export class TasksService {
   constructor(
     @InjectRepository(TaskEntity)
     private readonly taskRepository: Repository<TaskEntity>,
+    private readonly dataSource: DataSource,
   ) {}
 
   //injet our repository
@@ -101,20 +103,66 @@ export class TasksService {
     task.status = TaskStatus.OPEN;
     task.user = user;
 
+    const task2 = new TaskEntity();
+    task2.title = 'Task ENTRADO HARD CODED';
+    task2.description = 'Task ENTRADO HARD CODED';
+    task2.status = TaskStatus.OPEN;
+    // task2.user = user;
+    task2.userId = 1;
+
+    class t {
+      nombre = 'sd';
+    }
+
+    const userl = new t();
+    userl.nombre = 'password';
+
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    //open transaction
+    // await queryRunner.startTransaction('SERIALIZABLE');
+    await queryRunner.startTransaction();
     try {
-      const saved: TaskEntity = await this.taskRepository.save({ ...task }); //insert or update
+      // const saved: TaskEntity = await this.taskRepository.save({ ...task }); //insert or update
+
+      console.log('MICHAEL ', task);
+      //do operation
+      // const saved: TaskEntity =await queryRunner.manager.save({ ...task });
+      const saved: TaskEntity = await queryRunner.manager.save(task);
+
+      // //ESTO NO FUNCIONO PARA TIRAR UN ERROR PORQUE LO USO CON AWait Y POR ESO NO FUNCIONA
+      // const a: number = 1;
+      // const b: number = 0;
+      // let c = 0;
+      // c = a / b;
+      // if (c !== 0) console.log(c);
+      // else {
+      //   throw new Error('Division by zero!');
+      // }
+      // await queryRunner.manager.save(task2);
+      //este es para emitir un error y no guardar  el anterior tarea.
+      // await queryRunner.manager.save(userl);
+
+      // await queryRunner.manager.save(task2);
       // const saved: InsertResult = await this.taskRepository.insert({ ...task }); //just to insert
       // console.log(saved);
       // return new Promise(() => {});
       //delete the user before sending back to the client
-      delete saved.user;
+      // delete saved.user;
+      //COMMIT TRANSACTION
+      await queryRunner.commitTransaction();
       return saved;
     } catch (error) {
+      //since we have erros let's rollback the changes we made
+      await queryRunner.rollbackTransaction();
       this.logger.error(
         `Failed to create task for user "${user.username}". Data:${JSON.stringify(createTaskDto)}`,
         error.stack,
       );
-      throw new InternalServerErrorException();
+      throw new InternalServerErrorException('Probmae', error.stack);
+    } finally {
+      //you need to release query runner which is manully created
+      await queryRunner.release();
     }
   }
 
